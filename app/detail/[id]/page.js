acfafdb64e0ed27c125a9c5d2351570eed4c1de6
zuu3 <ojh27467242@gmail.com>
2025-03-19 09:40:19 +0900

    feat :: 상세페이지 만들기 1 (Dynamic route)

---
+++ app/detail/[id]/page.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function Detail(props) {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").findOne({ _id: new ObjectId(props.params.id) });
+  return (
+    <div>
+      <h4>상세페이지</h4>
+      <div>{result.title}</div>
+      <div>{result.content}</div>
+    </div>
+  )
+}
--- app/list/page.js
+++ app/list/page.js
@@ -4,13 +4,12 @@ export default async function List() {
 
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  
   return (
     <div className="list-bg">
       {result.map((post, Idx) => {
         return (
           <div className="list-item" key={Idx}>
-            <h4>{post.title}</h4>
+            <h4><a href={`/detail/${post._id}`}>{post.title}</a></h4>
             <p>{post.content}</p>
           </div>
         )
